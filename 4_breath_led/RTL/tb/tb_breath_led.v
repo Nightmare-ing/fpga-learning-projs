a61935aede80bc04dabab0c089cc3d791b03d0bb
Smile <kingsanhu@gmail.com>
2024-01-09 19:51:57 +0800

    Finish tb module

---
+++ 4_breath_led/RTL/tb/tb_breath_led.v
@@ -0,0 +1,26 @@
+`timescale 1ns/1ns
+
+module tb_breath_led ();
+
+reg [0:0] sys_clk;
+reg [0:0] sys_rst;
+
+wire [0:0] led;
+
+parameter CLK_PERIOD = 20;
+
+initial begin
+    sys_clk <= 1'b0;
+    sys_rst <= 1'b0;
+    #200
+    sys_rst <= 1'b1;
+end
+
+always #(CLK_PERIOD/2) sys_clk <= ~sys_clk;
+
+breath_led breath_led_inst(
+    .sys_clk (sys_clk),
+    .sys_rst (sys_rst),
+    .led (led)
+)
+endmodule
