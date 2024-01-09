b72c67135c9496e49a3142f87cbbd776d8445620
Smile <kingsanhu@gmail.com>
2024-01-09 20:50:54 +0800

    Fix bugs
    - Should count 2us, 2ms, 2s.
    - Upper bound for cnt_2us should be 99, etc.

--- 4_breath_led/RTL/breath_led.v
+++ 4_breath_led/RTL/breath_led.v
@@ -4,37 +4,53 @@ module breath_led (
     output reg [0:0] led
 );
 
-parameter CNT_US_MAX = 7'd100;
-parameter CNT_MS_MAX = 10'd1000;
+parameter CNT_2US_MAX = 7'd100;
+parameter CNT_2MS_MAX = 10'd1000;
+parameter CNT_2S_MAX = 10'd1000;
 
-reg [6:0] cnt_us;
-reg [9:0] cnt_ms;
+reg [6:0] cnt_2us;
+reg [9:0] cnt_2ms;
+reg [9:0] cnt_2s;
 reg [0:0] proc_flag;
 
-/* count 2ms with cnt_us */
+/* count 2us with cnt_2us */
 always @(posedge sys_clk or negedge sys_rst) begin
     if (!sys_rst) begin
-        cnt_us <= 7'd0;
+        cnt_2us <= 7'd0;
     end
-    if (cnt_us < CNT_US_MAX) begin
-        cnt_us <= cnt_us + 7'b1;
+    if (cnt_2us < CNT_2US_MAX - 7'd1) begin
+        cnt_2us <= cnt_2us + 7'd1;
     end else begin
-        cnt_us <= 7'd0;
+        cnt_2us <= 7'd0;
     end
 end
 
-/* count 2s with cnt_ms */
+/* count 2ms with cnt_2ms */
 always @(posedge sys_clk or negedge sys_rst) begin
     if (!sys_rst) begin
-        cnt_ms <= 10'd0;
-        proc_flag <= 1'd0;
+        cnt_2ms <= 10'd0;
     end
-    if (cnt_ms < CNT_MS_MAX) begin
-        if (cnt_us == CNT_US_MAX - 7'd1)begin
-            cnt_ms <= cnt_ms + 10'd1;
+    if (cnt_2ms < CNT_2MS_MAX - 10'd1) begin
+        if (cnt_2us == CNT_2US_MAX - 7'd1)begin
+            cnt_2ms <= cnt_2ms + 10'd1;
         end
     end else begin
-        cnt_ms <= 10'd0;
+        cnt_2ms <= 10'd0;
+    end
+end
+
+/* count 2s with cnt_2s */
+always @(posedge sys_clk or negedge sys_rst) begin
+    if (!sys_rst) begin
+        cnt_2s <= 10'd0;
+        proc_flag <= 1'd0;
+    end
+    if (cnt_2s < CNT_2S_MAX - 10'd1) begin
+        if (cnt_2ms == CNT_2MS_MAX - 10'd1) begin
+            cnt_2s <= cnt_2s + 10'd1;
+        end
+    end else begin
+        cnt_2s <= 10'd0;
         proc_flag <= ~proc_flag;
     end
 end
@@ -44,7 +60,7 @@ always @(posedge sys_clk or negedge sys_rst) begin
     if (!sys_rst) begin
         led <= 1'd0;
     end
-    if (cnt_us < cnt_ms) begin
+    if (cnt_2ms < cnt_2s) begin
         led <= ~proc_flag;
     end else begin
         led <= proc_flag;
